2128f373cf930bea2eaaaf09595a5fb1bdf9d3e3
Rocco Losito <roccolosito@gmail.com>
2020-06-18 14:43:06 -0400

    todaysWeather function working, need to add rest of the city data & styling

--- Master/script.js
+++ Master/script.js
@@ -1,30 +1,38 @@
 $(document).ready(function () {
 
-    var apiKey = "85a681bb50b1efa62965db606c2a91cd";
-    var currentUrl = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=" + apiKey;
+    // var apiKey = "85a681bb50b1efa62965db606c2a91cd";
+    // var currentUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=85a681bb50b1efa62965db606c2a91cd";
     // var fiveDayUrl = "https://api.openweathermap.org/data/2.5/forecast?APPID=" + apiKey;
     // var uvUrl = "https://api.openweathermap.org/data/2.5/uvi?APPID=" + apiKey;
 
-    $('#search-button').click(function (e) {
-        console.log('button clicked');
-        event.preventDefault();
-        var city = $("#search-value").val();
-        // localStorage.setItem("lastCity", city);
-        // $(".list-group").prepend($("<button>").addClass("list-group-item").text(city));
-        // displayCityInfo(city);
-    });
-
     // Function to retrieve and set current weather for city
-    // function todaysWeather(city) {
+    function todaysWeather(city) {
+
+        var currentUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=85a681bb50b1efa62965db606c2a91cd";
         $.ajax({
             url: currentUrl,
             method: "GET"
         }).then(function (response) {
-            // console.log(currentUrl);
-            console.log(response.main.temp);
+            console.log(response);
+            // Constructing HTML containing the today's weather info 
+            var cityName = $("<h3>").text(response.name);
+
+            // Empty the contents of the 'today' div, append the new weather data
+            $("#today").empty();
+            $("#today").append(cityName);
 
         })
-    // }
+    }
+
+    $('#search-button').click(function (e) {
+        console.log('button clicked');
+        event.preventDefault();
+        var city = $("#search-value").val().trim();
+        localStorage.setItem("lastCity", city);
+        // $(".list-group").prepend($("<button>").addClass("list-group-item").text(city));
+        todaysWeather(city);
+    });
+
 
     function tempConvertK2F(kelvin) {
         return ((kelvin - 273.15) * 9 / 5) + 32;
