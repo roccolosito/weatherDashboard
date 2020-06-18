43eb1f27283f4fca27edb2f14893789be369aadc
Rocco Losito <roccolosito@gmail.com>
2020-06-18 15:21:13 -0400

    todaysWeather section almost done, working on retrieving UV index

--- Master/script.js
+++ Master/script.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
 
-    // var apiKey = "85a681bb50b1efa62965db606c2a91cd";
-    // var currentUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=85a681bb50b1efa62965db606c2a91cd";
     // var fiveDayUrl = "https://api.openweathermap.org/data/2.5/forecast?APPID=" + apiKey;
     // var uvUrl = "https://api.openweathermap.org/data/2.5/uvi?APPID=" + apiKey;
 
@@ -14,12 +12,16 @@ $(document).ready(function () {
             method: "GET"
         }).then(function (response) {
             console.log(response);
-            // Constructing HTML containing the today's weather info 
-            var cityName = $("<h3>").text(response.name);
+            // Constructing HTML containing today's weather info 
+            var cityName = $("<h4>").text(response.name);
+            var todaysDate = moment().format('LL');
+            var weatherImage = $("<img>").attr("src", "http://openweathermap.org/img/wn/"+response.weather[0].icon+"@2x.png");
+            var currentTemp = $("<p>").text(tempConvertK2F(response.main.temp) + " °F");
+            var humidity = $("<p>").text(response.main.humidity);
 
             // Empty the contents of the 'today' div, append the new weather data
             $("#today").empty();
-            $("#today").append(cityName);
+            $("#today").append(cityName, todaysDate, weatherImage, currentTemp, humidity);
 
         })
     }
@@ -35,6 +37,6 @@ $(document).ready(function () {
 
 
     function tempConvertK2F(kelvin) {
-        return ((kelvin - 273.15) * 9 / 5) + 32;
+        return (((parseFloat(kelvin) - 273.15) * (9/5)) + 32).toFixed(1);
     }
 })
