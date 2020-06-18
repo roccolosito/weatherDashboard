ab17164dd3de6acb599a6a7181533c0a0e41edd0
Rocco Losito <roccolosito@gmail.com>
2020-06-18 15:42:05 -0400

    more UV work done

--- Master/script.js
+++ Master/script.js
@@ -7,6 +7,7 @@ $(document).ready(function () {
     function todaysWeather(city) {
 
         var currentUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=85a681bb50b1efa62965db606c2a91cd";
+
         $.ajax({
             url: currentUrl,
             method: "GET"
@@ -15,7 +16,7 @@ $(document).ready(function () {
             // Constructing HTML containing today's weather info 
             var cityName = $("<h4>").text(response.name);
             var todaysDate = moment().format('LL');
-            var weatherImage = $("<img>").attr("src", "http://openweathermap.org/img/wn/"+response.weather[0].icon+"@2x.png");
+            var weatherImage = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png");
             var currentTemp = $("<p>").text(tempConvertK2F(response.main.temp) + " °F");
             var humidity = $("<p>").text(response.main.humidity);
 
@@ -24,6 +25,10 @@ $(document).ready(function () {
             $("#today").append(cityName, todaysDate, weatherImage, currentTemp, humidity);
 
         })
+    }
+
+    function tempConvertK2F(kelvin) {
+        return (((parseFloat(kelvin) - 273.15) * (9 / 5)) + 32).toFixed(1);
     }
 
     $('#search-button').click(function (e) {
@@ -35,8 +40,15 @@ $(document).ready(function () {
         todaysWeather(city);
     });
 
+    // function getUVIndex(lat, lon) {
 
-    function tempConvertK2F(kelvin) {
-        return (((parseFloat(kelvin) - 273.15) * (9/5)) + 32).toFixed(1);
-    }
+    //     var uvUrl = "https://api.openweathermap.org/data/2.5/uvi?q=" + lat + lon + "&appid=85a681bb50b1efa62965db606c2a91cd";
+    //     $.ajax({
+    //         url: uvUrl,
+    //         method: "GET"
+    //     }).then(function (response) {
+
+    //     })
+    // }
+
 })
