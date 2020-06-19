483d0b00396da7af5fabfbf127fc444528039fc8
Rocco Losito <roccolosito@gmail.com>
2020-06-18 22:07:26 -0400

    more uv work done

--- Master/script.js
+++ Master/script.js
@@ -12,17 +12,19 @@ $(document).ready(function () {
             url: currentUrl,
             method: "GET"
         }).then(function (response) {
-            console.log(response);
+            // console.log(response);
             // Constructing HTML containing today's weather info 
             var cityName = $("<h4>").text(response.name);
+            console.log(cityName);
             var todaysDate = moment().format('LL');
             var weatherImage = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png");
             var currentTemp = $("<p>").text(tempConvertK2F(response.main.temp) + " °F");
             var humidity = $("<p>").text(response.main.humidity);
+            var uvIndex = $("span").text(getUVIndex(response.coord.lat, response.coord.lon));
 
             // Empty the contents of the 'today' div, append the new weather data
             $("#today").empty();
-            $("#today").append(cityName, todaysDate, weatherImage, currentTemp, humidity);
+            $("#today").append(cityName, todaysDate, weatherImage, currentTemp, humidity, uvIndex);
 
         })
     }
@@ -40,15 +42,30 @@ $(document).ready(function () {
         todaysWeather(city);
     });
 
-    // function getUVIndex(lat, lon) {
+    function getUVIndex(lat, lon) {
 
-    //     var uvUrl = "https://api.openweathermap.org/data/2.5/uvi?q=" + lat + lon + "&appid=85a681bb50b1efa62965db606c2a91cd";
-    //     $.ajax({
-    //         url: uvUrl,
-    //         method: "GET"
-    //     }).then(function (response) {
-
-    //     })
-    // }
+        var uvUrl = "https://api.openweathermap.org/data/2.5/uvi?q=" + lat + lon + "&appid=85a681bb50b1efa62965db606c2a91cd";
+        $.ajax({
+            url: uvUrl,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response)
+            var uvIndex = parseFloat(response.value).toFixed(2);
+            var p = $("<p>");
+            var span = $("<span>");
+            // span.css("background-color", color);
+            // span.css("padding", "5px");
+            span.text(uvIndex);
+            p.append("UV Index: ").append(span);
+            $("#today").append(p);
+            if(uvIndex >= 3){
+                span.addClass("favorable");
+            } else if (uvIndex >= 6){
+                span.addClass("moderate");
+            } else {
+                span.addClass("severe");
+            }
+        })
+    }
 
 })
